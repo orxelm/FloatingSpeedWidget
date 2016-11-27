e65141281506f55a7b67d0423eae94694bc95a51
Or Elmaliah <or@bringg.com>
2016-11-27 11:40:47 +0200

    added empty project

---
+++ FloatingSpeedWidget/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FloatingSpeedWidget
+//
+//  Created by Or Elmaliah on 27/11/2016.
+//  Copyright © 2016 Or Elmaliah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
